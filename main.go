297898ccb045b5b624321aaa18be1a926906bed6
natchaphonbw <natchaphon.bw@gmail.com>
2025-06-13 15:09:44 +0700

    Remove unused Fiber import and default route handler from main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/natchaphonbw/usermanagement/config"
 	"github.com/natchaphonbw/usermanagement/pkg/databases"
 	"github.com/natchaphonbw/usermanagement/pkg/databases/migrations"
@@ -21,15 +20,6 @@ func main() {
 	app := server.NewFiberApp()
 
 	server.SetupRoutes(app, db)
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"fiberHost": cfg.FiberHost,
-			"fiberPort": cfg.FiberPort,
-			"dbHost":    cfg.DBHost,
-			"dbPort":    cfg.DBPort,
-		})
-	})
 
 	addr := fmt.Sprintf("%s:%s", cfg.FiberHost, cfg.FiberPort)
 	app.Listen(addr)
